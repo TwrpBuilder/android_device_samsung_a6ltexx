5ede8812c26f9f1b4d050894c6d6387b751ec5aa
Surendra Kumar <sk2812283@gmail.com>
2018-07-12 22:24:09 +0530

    remove kernel cmdline aka permisive shit no nedeed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,7 +32,7 @@ TARGET_CPU_CORTEX_A53 := true
 
 # Kernel
 TARGET_PREBUILT_KERNEL := device/samsung/a6ltexx/kernel
-BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
+#BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/a6ltexx/dt.img
 
